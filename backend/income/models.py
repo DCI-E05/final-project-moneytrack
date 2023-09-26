e31b6161985de04e7f203226ce99263898f36eeb
swarna-kollu <kolluswarna@gmail.com>
2023-09-26 13:15:41 +0200

    check

--- backend/income/models.py
+++ backend/income/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from categories.models import Category
+
 # Create your models here.
 class Income(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
