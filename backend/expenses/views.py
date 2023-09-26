91353bc3ef1e26ec00743fdeb778a15d06763398
Maksym Marchuk <maxxik007@gmail.com>
2023-09-26 14:21:37 +0200

    [ADD]: Configured ViewSet for Expense model.

--- backend/expenses/views.py
+++ backend/expenses/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import ExpenseSerializer
+from .models import Expense
 
-# Create your views here.
+
+class ExpensesViewSet(viewsets.ModelViewSet):
+    serializer_class = ExpenseSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = Expense.objects.all()
+
+    def get_queryset(self):
+        user = self.request.user
+        query_set = self.queryset
+        return query_set.filter(user=user)
+
