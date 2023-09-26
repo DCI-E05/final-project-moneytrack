a902263b6ca96d32fc5f222ef96e65cd4168a640
swarna-kollu <kolluswarna@gmail.com>
2023-09-26 13:02:24 +0200

    create model in income app

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "expenses",
     "income",
     
+    
 ]
 
 MIDDLEWARE = [
--- backend/income/models.py
+++ backend/income/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from categories.models import Category
 # Create your models here.
+class Income(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.PositiveIntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)  # Define Category model before using it
+    date = models.DateField()
+    description = models.TextField()
+
+    def __str__(self):
+        return f"{self.user.username}'s Income - {self.date}"
+    
+
+
