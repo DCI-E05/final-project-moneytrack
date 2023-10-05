0969321fc88675c95e3d30211189bcbfe3a8b63b
1bless <a.salloum@gmx.de>
2023-10-05 13:24:22 +0200

    views in expenses and income are only pure api now, i hope... ,remove templates redirects

--- backend/income/views.py
+++ backend/income/views.py
@@ -1,14 +1,12 @@
 from .models import Income
 from .serializers import IncomeSerializer
-from rest_framework import viewsets
-from rest_framework import permissions
-from django.shortcuts import render, redirect
+from rest_framework import viewsets, status, permissions
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from django.conf import settings
 from mindee import Client, documents
-from django.contrib.auth.decorators import login_required
 from .forms import DocUploadForm
 import csv
-from django.contrib import messages
 
 class IncomeViewSet(viewsets.ModelViewSet):
     serializer_class = IncomeSerializer
@@ -20,37 +18,24 @@ class IncomeViewSet(viewsets.ModelViewSet):
         query_set = self.queryset
         return query_set.filter(user=user)
 
+    @action(detail=False, methods=['POST'], permission_classes=[permissions.IsAuthenticated])
+    def upload_receipt(self, request):
+        form = DocUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            receipt_image = request.FILES['receipt_image']
+            if not receipt_image.content_type.startswith('image/'):
+                return Response({"error": "Invalid file type. Please upload an image."}, status=status.HTTP_400_BAD_REQUEST)
 
-class IncomeReceiptUpload(viewsets.ModelViewSet):
+            try:
+                process_mindee_api(receipt_image, request)
+            except Exception as e:
+                return Response({"error": f"Error with Mindee API: {e}"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-    def success_view(request):
-        return render(request, 'success.html') #will configure this after frontend is ready
+            return Response({"message": "Income receipt processed successfully."}, status=status.HTTP_200_OK)
 
-    @login_required
-    def upload_income_receipt(request):
-        if request.method == 'POST':
-            form = DocUploadForm(request.POST, request.FILES)
-            if form.is_valid():
-                receipt_image = request.FILES['receipt_image'] #will configure this after frontend is ready
-                if not receipt_image.content_type.startswith('image/'):
-                    messages.error(request, "Invalid file type. Please upload an image.")
-                    return render(request, 'upload_income_receipt.html', {'form': form})
+        return Response({"error": "Invalid form submission."}, status=status.HTTP_400_BAD_REQUEST)
 
-                try:
-                    process_mindee_api_for_income(receipt_image, request)
-                except Exception as e:
-                    messages.error(request, f"Error with Mindee API: {e}")
-                    return render(request, 'upload_income_receipt.html', {'form': form})
-
-                return redirect('success_view')
-
-        else:
-            form = DocUploadForm()
-
-        return render(request, 'upload_income_receipt.html', {'form': form})
-
-
-def process_mindee_api_for_income(receipt_image, request):
+def process_mindee_api(receipt_image, request):
     mindee_client = Client(api_key=settings.MINDEE_API_KEY)
     input_doc = mindee_client.doc_from_file(receipt_image)
     result = input_doc.parse(documents.TypeInvoiceV4)
@@ -68,8 +53,8 @@ def process_mindee_api_for_income(receipt_image, request):
 
 
 def write_to_income_csv(username, supplier_name, invoice_date, total_amount):
-    with open(settings.CSV_FILE_income, 'a', newline='') as csvfile:
-        fieldnames = ['user', 'supplier_name', 'invoice_date', 'total_amount']
+    with open(settings.CSV_FILE_INCOME, 'a', newline='') as csvfile:
+        fieldnames = ['user', 'income_source', 'date', 'amount']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
         if csvfile.tell() == 0:
